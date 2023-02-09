485ae8a76214a09fa38bf23de73405268f2602b6
Danilo Silva <danilo.silva@msn.com>
2023-02-09 07:32:17 -0300

    Simulando o Pedido

--- BaltaStore/BaltaStore.Testes/UnitTest1.cs
+++ BaltaStore/BaltaStore.Testes/UnitTest1.cs
@@ -1,4 +1,5 @@
 using BaltaStore.Dominio.Entidades;
+using BaltaStore.Dominio.ValueObjects;
 
 namespace BaltaStore.Testes;
 
@@ -8,7 +9,28 @@ public class UnitTest1
     [TestMethod]
     public void TestMethod1()
     {
-        // var cliente = new Cliente("Danilo", "Silva", "33311100033", "danilo.silva@msn.com", "11996662200";
-        // var pedido = new Pedido(cliente);
+        var nome = new Nome("Danilo", "Silva");
+        var documento = new Documento("12345678911");
+        var email = new Email("danilo.silva@msn.com");
+        var cliente = new Cliente(nome, documento, email, "11996662200");
+
+        var mouse = new Produto("Mouse", "Mouse", "Imagem.png", 59.90M, 10);
+        var teclado = new Produto("Reclado", "Reclado", "Imagem.png", 159.90M, 10);
+        var impressora = new Produto("Impressora", "Impressora", "Imagem.png", 359.90M, 10);
+        var cadeira = new Produto("Cadeira", "Cadeira", "Imagem.png", 559.90M, 10);
+
+        var pedido = new Pedido(cliente);
+        pedido.adicionarItem(new ItemPedido(mouse, 5));
+        pedido.adicionarItem(new ItemPedido(teclado, 5));
+        pedido.adicionarItem(new ItemPedido(cadeira, 5));
+        pedido.adicionarItem(new ItemPedido(impressora, 5));
+
+        pedido.Fechar();
+
+        pedido.Pagar();
+
+        pedido.Enviar();
+
+        pedido.Cancelar();
     }
 }
