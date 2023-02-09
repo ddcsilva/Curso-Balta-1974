51a9c4838c4406e1b7f7f7272b8433aee7eb9fc5
Danilo Silva <danilo.silva@msn.com>
2023-02-09 05:53:52 -0300

    Testes Básicos

--- BaltaStore/BaltaStore.Testes/UnitTest1.cs
+++ BaltaStore/BaltaStore.Testes/UnitTest1.cs
@@ -1,3 +1,5 @@
+using BaltaStore.Dominio.Entidades;
+
 namespace BaltaStore.Testes;
 
 [TestClass]
@@ -6,5 +8,7 @@ public class UnitTest1
     [TestMethod]
     public void TestMethod1()
     {
+        var cliente = new Cliente("Danilo", "Silva", "33311100033", "danilo.silva@msn.com", "11996662200", "Rua XV de Novembro");
+        var pedido = new Pedido(cliente);
     }
 }
