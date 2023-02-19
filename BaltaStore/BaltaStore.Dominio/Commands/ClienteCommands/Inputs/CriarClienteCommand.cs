53a5ec097fff645320b2becf5df71a34aef9ea50
Danilo Silva <danilo.silva@msn.com>
2023-02-19 20:30:28 -0300

    Aplicando Validações

--- BaltaStore/BaltaStore.Dominio/Commands/ClienteCommands/Inputs/CriarClienteCommand.cs
+++ BaltaStore/BaltaStore.Dominio/Commands/ClienteCommands/Inputs/CriarClienteCommand.cs
@@ -1,11 +1,28 @@
+using BaltaStore.Comum.Commands;
+using FluentValidator;
+using FluentValidator.Validation;
+
 namespace BaltaStore.Dominio.Commands.CustomerCommands.Inputs
 {
-    public class CriarClienteCommand
+  public class CriarClienteCommand : Notifiable, ICommand
+  {
+    public string? PrimeiroNome { get; set; }
+    public string? Sobrenome { get; set; }
+    public string? Documento { get; set; }
+    public string? Email { get; set; }
+    public string? Telefone { get; set; }
+
+    public bool Valido()
     {
-        public string? PrimeiroNome { get; set; }
-        public string? Sobrenome { get; set; }
-        public string? Documento { get; set; }
-        public string? Email { get; set; }
-        public string? Telefone { get; set; }
+      AddNotifications(new ValidationContract()
+                  .HasMinLen(PrimeiroNome, 3, "Primeiro Nome", "O nome deve conter pelo menos 3 caracteres")
+                  .HasMaxLen(PrimeiroNome, 40, "Primeiro Nome", "O nome deve conter pelo menos 40 caracteres")
+                  .HasMinLen(Sobrenome, 3, "Sobrenome", "O sobrenome deve conter pelo menos 3 caracteres")
+                  .HasMaxLen(Sobrenome, 40, "Sobrenome", "O sobrenome deve conter pelo menos 40 caracteres")
+                  .HasLen(Documento, 11, "Documento", "CPF Inválido")
+              );
+
+      return Valido();
     }
+  }
 }
--- BaltaStore/BaltaStore.Dominio/Commands/PedidoCommands/Inputs/CriarItemPedidoCommand.cs
+++ BaltaStore/BaltaStore.Dominio/Commands/PedidoCommands/Inputs/CriarItemPedidoCommand.cs
@@ -1,8 +1,16 @@
+using BaltaStore.Comum.Commands;
+using FluentValidator;
+
 namespace BaltaStore.Dominio.Commands.PedidoCommands.Inputs
 {
-    public class CriarItemPedidoCommand
+    public class CriarItemPedidoCommand : Notifiable, ICommand
     {
         public Guid Produto { get; set; }
         public decimal Quantidade { get; set; }
+
+    public bool Valido()
+    {
+      throw new NotImplementedException();
     }
+  }
 }
