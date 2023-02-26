7b026c97beea0d2fccaa840e50a15b7ec24a7ed9
Danilo Silva <danilo.silva@msn.com>
2023-02-26 00:23:23 -0300

    Repositórios

---
+++ BaltaStore/BaltaStore.Dominio/Repositories/IClienteRepository.cs
@@ -0,0 +1,10 @@
+﻿using BaltaStore.Dominio.Entidades;
+
+namespace BaltaStore.Dominio.Repositories;
+
+public interface IClienteRepository
+{
+    bool VerificarDocumento(string documento);
+    bool VerificarEmail(string email);
+    void Gravar(Cliente cliente);
+}
